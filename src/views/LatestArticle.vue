48e631127c087d62e1d28f0f3a4a9eedb36c3fe0
GITHUB <devninja.up@gmail.com>
2023-03-09 00:49:37 +0900

    add

--- src/views/LatestArticle.vue
+++ src/views/LatestArticle.vue
@@ -15,17 +15,15 @@ export default {
 </script>
 
 <template>
-  <div class="w-full px-28 flex">
-    <text class="text-[white] font-extrabold text-6xl mt-[183px]">
-      Media ENT
-    </text>
-    <div class="w-[570px] h-[132px] mt-[67px] text-[white] font-medium text-topSection">
-      Platfom media <text class="text-[#1BB3B5]">
-        Jurnalistik
-      </text> yang memberi informasi seputar kampus, teknologi, dan berita terkini.
+  <div class="w-full px-28 flex flex-col">
+    <div class="text-[white] font-bold text-3xl mt-[100px]">
+        Artikel Terbaru
     </div>
-    <button class='w-[203px] h-[57px] rounded-[10px] font-semibold text-xl bg-[white]'>
-      About <text class="text-[#0C4A6E]">ENT</text>
-    </button>
+    <div class="mt-[4px] text-[white] font-normal text-xl">
+        Kumpulan artikel terbaru seputar kampus, teknologi, dan tren terkini.
+    </div>
+    <div class="gap-5">
+
+    </div>
   </div>
 </template>
